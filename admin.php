a05643785675a242c05231f204b34fd7b72ecbf4
YventoZ <62593110+YventoZ@users.noreply.github.com>
2020-05-24 22:20:49 +0200

    Add files via upload
    Försök på att ta bort data

---
+++ admin.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "127.0.0.1";
+$username = "j";
+$password = "j";
+$dbname = "carcharts";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // sql to delete a record
+  $sql = $_POST ["DELETE FROM login WHERE user_id=3"];
+
+  // use exec() because no results are returned
+  $conn->exec($sql);
+    echo "Record deleted successfully";
+} catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
